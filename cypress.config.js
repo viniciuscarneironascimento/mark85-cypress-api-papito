933d5dff37a8f0e278c2166e8a518e27948502ea
Vinicius <contato.vinicius@gmail.com>
2025-04-03 10:58:18 -0300

    Reinstalacao mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,17 @@ const { connect } = require('./cypress/support/mongo')
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions:{
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json:false,
+      timestamp: "mmddyyy_HHMMss"}
     /*async setupNodeEvents(on, config) {
 
       const db = await connect()
